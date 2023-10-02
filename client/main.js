067f5f9e723fd1f1e224cbd705b219fe5982d7bc
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2023-10-03 02:23:15 +0530

    Adding and previewing of file

---
+++ client/main.js
@@ -0,0 +1,31 @@
+function openFileInput() {
+  const fileInput = document.createElement("input")
+  fileInput.type = "file"
+  fileInput.style.display = "none"
+  document.body.appendChild(fileInput)
+  fileInput.click()
+
+  fileInput.addEventListener("change", () => {
+    const fileInputName = document.querySelector(".image-upload-input")
+    fileInputName.value = fileInput.value.split("\\").pop()
+    handlePreviewOfImage(fileInput)
+    document.body.removeChild(fileInput)
+  })
+}
+
+function handlePreviewOfImage(fileInput) {
+  previewImage = document.querySelector(".preview-image")
+  imagePreview = document.querySelector(".image-preview")
+  file = fileInput.files[0]
+
+  if (file) {
+    const fileReader = new FileReader()
+    fileReader.onload = function (e) {
+      imagePreview.src = e.target.result
+      previewImage.style.display = "block"
+    }
+    fileReader.readAsDataURL(file)
+  } else {
+    previewImage.style.display = "none"
+  }
+}
